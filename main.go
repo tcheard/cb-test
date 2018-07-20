6de5430ecdb45e3f43a5da8ac0df1ca47b818521
Thomas Heard <tom.heard@tracplus.com>
2018-07-20 16:48:58 +1200

    Hello -> Kia ora

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+		fmt.Fprintf(w, "Kia ora, you've requested: %s\n", r.URL.Path)
 	})
 
 	http.ListenAndServe(":80", nil)
